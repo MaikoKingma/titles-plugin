053a9c0960f72b878b2322b59db91239ca84a8f7
Maiko Kingma <maiko.kingma@outlook.com>
2017-09-09 16:54:12 +0200

    Implemented all current commands

--- commandManager.ts
+++ commandManager.ts
@@ -1,4 +1,10 @@
+import { ChatRegistry } from "./chat/chat-registry";
+import { Chat } from "./chat/chat";
+import { Title } from "./title/title";
+
 export class CommandManager {
+
+    constructor(private readonly chatRegistry: ChatRegistry = new ChatRegistry) { }
 
     /**
      * Adds a title to the plugin.
@@ -14,7 +20,7 @@ export class CommandManager {
         // Identify and verify arguments.
         const minRange = Number(split[1]);
         const maxRange = Number(split[2]);
-        const name = split.slice(3).join;
+        const name = split.slice(3).join + "";
 
         if (isNaN(minRange)) {
             return "The minRange must be a number!";
@@ -23,9 +29,13 @@ export class CommandManager {
             return "The maxRange must be a number!";
         }
 
-        //ToDo implement addTitle
+        //Add the new title to the given chat
+        const chat =  this.chatRegistry.getOrCreateChat(chatID);
+        if (chat.addTitle(name, minRange, maxRange)) {
+            return "Added the new title!";
+        }
 
-        return "CommandNotImplementedException";
+        return "Titles cannot overlap.";
     }
 
 
@@ -44,7 +54,7 @@ export class CommandManager {
         const id = Number(split[1]);
         const minRange = Number(split[2]);
         const maxRange = Number(split[3]);
-        const name = split.slice(4).join;
+        const name = split.slice(4).join + "";
 
         if (isNaN(minRange)) {
             return "The minRange must be a number!";
@@ -53,19 +63,30 @@ export class CommandManager {
             return "The maxRange must be a number!";
         }
         
-        //ToDo implement modifyTitle
+        //Modify the title in the given chat
+        const chat =  this.chatRegistry.getOrCreateChat(chatID);
+        const title = new Title(id, name, minRange, maxRange);
+        if (chat.modifyTitle(title)) {
+            return "Modified the title!";
+        }
 
-        return "CommandNotImplementedException";
+        return "Id did not match with an existing title or the new title would overlap with an existing title.";
     }
 
     /**
      * Returns all titles
      */
     public getTitles(chatID: number): string {
+        //Get titles
+        const titles = this.chatRegistry.getOrCreateChat(chatID).titles;
         
-        //ToDo implement getTitles
+        //Create message with all titles
+        let message = "<b>--- TITLES ---</b>\n\n";
+        for (let title of titles) {
+            message += title.toString() + "\n";
+        }
 
-        return "CommandNotImplementedException";
+        return message;
     }
 
     /**
@@ -84,10 +105,12 @@ export class CommandManager {
         if (isNaN(id)) {
             return "The id must be a number!";
         }
-
-        //ToDo implement removeTitle
-        //What to do with ranges without title?
-
-        return "CommandNotImplementedException";
+        
+        //Remove title from the specified chat
+        if (this.chatRegistry.getOrCreateChat(chatID).removeTitle(id)) {
+            "Title removed!";
+        }
+        
+        return "Id did not match with an existing title.";
     }
 }
